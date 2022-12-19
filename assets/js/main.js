a0efb60d1d45501261e78648d542a43928dc4aba
Melissa Huang <melissahuang10@gmail.com>
2022-12-18 18:17:59 -0800

    misfit manager

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = resizePaw;
-window.onresize = resizePaw;
-function resizePaw() {
-    var container = document.getElementById("blueprint");
-    var parent = container.clientHeight;
-    var child = document.getElementById("paw");
-    child.style.clip = "rect(0px 1000px " + (parent - 5) + "px 0px)";
-}
